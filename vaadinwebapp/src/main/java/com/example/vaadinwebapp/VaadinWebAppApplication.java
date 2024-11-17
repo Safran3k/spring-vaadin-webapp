71feb4f01a67320c5fd416611a0bff503161a1ba
safran3k <futo.ricsi@gmail.com>
2024-11-17 11:17:37 +0100

    Initial commit

---
+++ vaadinwebapp/src/main/java/com/example/vaadinwebapp/VaadinWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaadinwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinWebAppApplication.class, args);
+	}
+
+}
---
+++ vaadinwebapp/src/test/java/com/example/vaadinwebapp/VaadinWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaadinwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
